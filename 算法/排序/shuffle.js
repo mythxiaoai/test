fbbb4a996dffdca1dfd26fb61124b87a1063ab57
mythxiaoai <774813571@qq.com>
2022-03-25 07:28:15 +0800

    add vido random jietu

--- 算法/排序/shuffle.js
+++ 算法/排序/shuffle.js
@@ -1,11 +1,12 @@
 let arr = [3, 2, 1, 6, 7];
 
-function shuffle(a) {
+function shuffle(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         let j = Math.floor(Math.random() * arr.length);
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
+    return arr
 }
 
 function isSorted(a) {
@@ -17,5 +18,22 @@ function isSorted(a) {
     return true;
 }
 
+function isSorted(a) {
+  for (let i = 0; i < a.length; i++) {
+      if (a[i - 1] > a[i]) {
+          return false;
+      }
+  }
+  return true;
+}
+
 while (!isSorted(arr)) shuffle(arr);
-console.log(arr);
+console.log(shuffle(arr));
+
+//
+function getRandom(arr,fixIndex){
+  let newArr = arr.filter((v,i)=>i!==fixIndex);
+  console.log(newArr);
+}
+
+getRandom(arr,3)
---
+++ 算法/排序/shuffleFixed.js
@@ -0,0 +1,37 @@
+
+
+function isEqueal(arr1,arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+          return false;
+      }
+  }
+  return true;
+}
+
+
+function shuffle(arr) {
+  if(arr.length===2){
+    [arr[1], arr[0]] = [arr[0], arr[1]];
+    return arr;
+  }
+    for (let i = 0; i < arr.length; i++) {
+        let j = Math.floor(Math.random() * arr.length);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr
+}
+
+function getRandom(arr,fixIndex){
+  if(arr.length<=2)return arr;
+  let newArr = arr.filter((v,i)=>i!==fixIndex);
+  let randomArr = shuffle([...newArr]);
+  while (isEqueal(randomArr,newArr)){
+    randomArr = shuffle([...newArr]);
+  }
+  randomArr.splice(fixIndex,0,arr[fixIndex]);
+  return randomArr;
+}
+let arr = [3, 2, 5, 1];
+console.log(getRandom(arr,3));
+
